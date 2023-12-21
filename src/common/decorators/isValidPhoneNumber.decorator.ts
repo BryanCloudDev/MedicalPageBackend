30e5b0e653a039e8fd7691456cb6e17cdfc2edaa
Bryan Portillo <bportillo701@gmail.com>
2023-12-20 19:39:05 -0600

    MEDB-27 Added custom decorator.
    Created validator for phone number

---
+++ src/common/decorators/isValidPhoneNumber.decorator.ts
@@ -0,0 +1,54 @@
+import {
+  ValidationArguments,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  isString,
+  registerDecorator
+} from 'class-validator'
+import { PhoneType } from '../types'
+
+@ValidatorConstraint({ name: 'isValidPhoneNumber', async: false })
+export class IsValidPhoneNumberConstraint
+  implements ValidatorConstraintInterface
+{
+  readonly regexForPhoneNumber = /^\d{1,15}$/
+
+  validate(value: string) {
+    return this.validateNumber(value)
+  }
+
+  defaultMessage({ value, constraints }: ValidationArguments) {
+    if (value.length < 8 || value.length > 15)
+      return 'Number must be up to 15 characters long'
+
+    const [type] = constraints
+
+    if (type === 'mobilePhone') {
+      return `Mobile phone number must be numeric characters only. Received: ${value}`
+    }
+
+    return `Phone number must be numeric characters only. Received: ${value}`
+  }
+
+  validateNumber(value: string) {
+    const MAX_CHAR_AMOUNT = 15
+    const MIN_CHAR_AMOUNT = 8
+
+    return (
+      isString(value) &&
+      this.regexForPhoneNumber.test(value) &&
+      (value.length >= MIN_CHAR_AMOUNT || value.length <= MAX_CHAR_AMOUNT)
+    )
+  }
+}
+
+export function IsValidPhoneNumber(type: PhoneType) {
+  return function (object: Record<string, any>, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: [type],
+      validator: IsValidPhoneNumberConstraint
+    })
+  }
+}
