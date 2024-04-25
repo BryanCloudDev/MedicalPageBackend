f5eb8c633d42b88bf2748649d205cef78b3924cf
Bryan Portillo <bportillo701@gmail.com>
2024-04-25 01:36:27 -0600

    MEDB-66 Added Review Dto

--- src/reviews/dto/create-review.dto.ts
+++ src/reviews/dto/create-review.dto.ts
@@ -1,1 +1,27 @@
-export class CreateReviewDto {}
+import {
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  Matches,
+  MaxLength
+} from 'class-validator'
+
+export class CreateReviewDto {
+  @IsString()
+  @MaxLength(500)
+  @IsNotEmpty()
+  comment: string
+
+  @IsUUID()
+  doctorId: string
+
+  @IsUUID()
+  patientId: string
+
+  @IsInt()
+  @Matches(/^(10|[1-9])$/, {
+    message: 'rating must be between 1 and 10'
+  })
+  rating: number
+}
