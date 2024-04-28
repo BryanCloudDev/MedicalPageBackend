e0db89066909ecc31b9827541cc1b357a6f5e5eb
Bryan Portillo <bportillo701@gmail.com>
2024-04-28 15:34:15 -0600

    MEDB-70 Added Create Appointment Dto

--- src/appointment/dto/create-appointment.dto.ts
+++ src/appointment/dto/create-appointment.dto.ts
@@ -1,1 +1,14 @@
-export class CreateAppointmentDto {}
+import { Type } from 'class-transformer'
+import { IsDate, IsOptional, IsString } from 'class-validator'
+import { IsLaterThan } from 'src/common/decorators'
+
+export class CreateAppointmentDto {
+  @IsDate()
+  @IsLaterThan()
+  @Type(() => Date)
+  appointmentDate: Date
+
+  @IsString()
+  @IsOptional()
+  notes?: string
+}
