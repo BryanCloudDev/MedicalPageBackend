9e43fabc8e4eb9f07e215f349583337f7a38da4c
Bryan Portillo <bportillo701@gmail.com>
2024-04-28 21:06:38 -0600

    MEDB-74 Added Create Hospital Dto

--- src/hospital/dto/create-hospital.dto.ts
+++ src/hospital/dto/create-hospital.dto.ts
@@ -1,1 +1,19 @@
-export class CreateHospitalDto {}
+import { Type } from 'class-transformer'
+import {
+  IsNotEmptyObject,
+  IsString,
+  Length,
+  ValidateNested
+} from 'class-validator'
+import { CreateAddressDto } from 'src/address/dto/address/create-address.dto'
+
+export class CreateHospitalDto {
+  @IsString()
+  @Length(3, 15)
+  name: string
+
+  @ValidateNested({ each: true })
+  @Type(() => CreateAddressDto)
+  @IsNotEmptyObject()
+  address: CreateAddressDto
+}
