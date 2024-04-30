82ff1cf038718a59125cb9ea82060463733bfaeb
Bryan Portillo <bportillo701@gmail.com>
2024-04-29 23:33:03 -0600

    MEDB-76 Added Hospital Service

--- src/hospital/hospital.module.ts
+++ src/hospital/hospital.module.ts
@@ -5,11 +5,13 @@ import { HospitalDoctors } from './entities/hospitalDoctor.entity'
 import { Hospital } from './entities/hospital.entity'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule } from '@nestjs/config'
+import { AddressModule } from 'src/address/address.module'
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Hospital, HospitalDoctors]),
-    ConfigModule
+    ConfigModule,
+    AddressModule
   ],
   controllers: [HospitalController],
   providers: [HospitalService]
