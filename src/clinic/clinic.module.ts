780b303dc23f92ab38c615b1896d4ef70980a042
Bryan Portillo <bportillo701@gmail.com>
2024-05-02 23:49:22 -0600

    MEDB-80 Added Clinic Service

--- src/clinic/clinic.module.ts
+++ src/clinic/clinic.module.ts
@@ -4,9 +4,15 @@ import { ClinicController } from './clinic.controller'
 import { Clinic } from './entities/clinic.entity'
 import { ClinicService } from './clinic.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
+import { AddressModule } from 'src/address/address.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Clinic, ClinicDoctor])],
+  imports: [
+    TypeOrmModule.forFeature([Clinic, ClinicDoctor]),
+    ConfigModule,
+    AddressModule
+  ],
   controllers: [ClinicController],
   providers: [ClinicService]
 })
