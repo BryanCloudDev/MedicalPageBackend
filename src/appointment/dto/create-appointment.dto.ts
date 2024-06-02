24397b3f1d057703c036660581fb86630748c3b7
Bryan Portillo <bportillo701@gmail.com>
2024-06-01 22:52:10 -0600

    MEDB-83 Added Appointment Documentation

--- src/appointment/dto/create-appointment.dto.ts
+++ src/appointment/dto/create-appointment.dto.ts
@@ -1,15 +1,26 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
 import { IsDate, IsOptional, IsString, IsUUID } from 'class-validator'
 
 export class CreateAppointmentDto {
+  @ApiProperty({ type: Date, example: '2024-06-30T09:00:00.000Z' })
   @IsDate()
   @Type(() => Date)
   appointmentDate: Date
 
+  @ApiProperty({
+    type: String,
+    required: false,
+    example: 'Bring your medical reports'
+  })
   @IsString()
   @IsOptional()
   notes?: string
 
+  @ApiProperty({
+    type: String,
+    example: 'f09fdcc9-0d71-402b-af5a-0b5f60793cb0'
+  })
   @IsUUID()
   doctorId: string
 }
--- src/appointment/dto/update-appointment.dto.ts
+++ src/appointment/dto/update-appointment.dto.ts
@@ -1,9 +1,10 @@
-import { PartialType } from '@nestjs/mapped-types'
 import { CreateAppointmentDto } from './create-appointment.dto'
 import { IsEnum } from 'class-validator'
 import { Status } from '../enums/status-appoinment.enum'
+import { ApiProperty, PartialType } from '@nestjs/swagger'
 
 export class UpdateAppointmentDto extends PartialType(CreateAppointmentDto) {
+  @ApiProperty({ enum: Status, example: Status.CANCELLED })
   @IsEnum(Status)
-  status?: string
+  status?: Status
 }
