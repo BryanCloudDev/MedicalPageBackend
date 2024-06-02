71b2f2a8fb635a1d97bab6f272f7da329d70e560
Bryan Portillo <bportillo701@gmail.com>
2024-06-02 01:41:12 -0600

    MEDB-83 Added FIle Retrieval Documentation

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,13 +1,31 @@
 import { Response } from 'express'
 import { Controller, Get, Logger, Param, Res } from '@nestjs/common'
 import { FileService } from './file.service'
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger'
+import { GenericResponses } from 'src/common/decorators/genericResponses.decorator'
 
+@ApiTags('File')
 @Controller('file')
 export class FileController {
   constructor(private readonly fileService: FileService) {}
   private readonly logger = new Logger(FileController.name)
 
   @Get(':folder/:fileName')
+  @GenericResponses()
+  @ApiOkResponse({
+    description: 'Success',
+    content: {
+      'application/octet-stream': {
+        schema: {
+          type: 'string',
+          format: 'binary'
+        }
+      }
+    }
+  })
+  @ApiOperation({
+    summary: 'Get user photo by folder and file name'
+  })
   async getFile(
     @Param('folder') folder: string,
     @Param('fileName') fileName: string,
