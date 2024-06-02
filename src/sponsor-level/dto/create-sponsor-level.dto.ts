bd8299b68b92532b9ee07a75646744c0ac115a65
Bryan Portillo <bportillo701@gmail.com>
2024-06-02 16:14:38 -0600

    MEDB-83 Added Sponsor Level Documentation

--- src/sponsor-level/dto/create-sponsor-level.dto.ts
+++ src/sponsor-level/dto/create-sponsor-level.dto.ts
@@ -1,19 +1,45 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { Type } from 'class-transformer'
 import { IsNumber, IsInt, IsString, Length } from 'class-validator'
+import { Subscription } from '../enum/subscription.enum'
 
 export class CreateSponsorLevelDto {
+  @ApiProperty({
+    enum: Subscription,
+    example: Subscription.BASIC,
+    description: 'Subscription type',
+    minimum: 5,
+    maximum: 25
+  })
   @IsString()
   @Length(5, 25)
-  name: string
+  name: Subscription
 
+  @ApiProperty({
+    example: 19.99,
+    description: 'Subscription price',
+    minimum: 0
+  })
   @IsNumber()
   @Type(() => Number)
   price: number
 
+  @ApiProperty({
+    example: 84600,
+    description: 'Subscription duration in seconds',
+    minimum: 1
+  })
   @IsInt()
   @Type(() => Number)
   duration: number
 
+  @ApiProperty({
+    example:
+      'El plan Essential Care es perfecto para médicos que recién comienzan en la plataforma.',
+    description: 'Subscription description',
+    minimum: 5,
+    maximum: 100
+  })
   @IsString()
   @Length(5, 100)
   description: string
--- src/sponsor-level/dto/update-sponsor-level.dto.ts
+++ src/sponsor-level/dto/update-sponsor-level.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types'
+import { PartialType } from '@nestjs/swagger'
 import { CreateSponsorLevelDto } from './create-sponsor-level.dto'
 
 export class UpdateSponsorLevelDto extends PartialType(CreateSponsorLevelDto) {}
