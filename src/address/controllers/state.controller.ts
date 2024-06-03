7c27a6eee8d3f20145f8272e015f135104ffb94b
Bryan Portillo <bportillo701@gmail.com>
2024-06-02 19:23:41 -0600

    MEDB-83 Added State Documentation

--- src/address/controllers/state.controller.ts
+++ src/address/controllers/state.controller.ts
@@ -16,27 +16,80 @@ import { PaginationDto } from 'src/common/dtos'
 import { UpdateStateDto } from '../dto/state/update-state.dto'
 import { Auth } from 'src/auth/decorators'
 import { Roles } from 'src/user/enums'
+import {
+  ApiCreatedResponse,
+  ApiExtraModels,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags
+} from '@nestjs/swagger'
+import { GenericResponses } from 'src/common/decorators/genericResponses.decorator'
+import { Description } from 'src/common/swagger/description.swagger'
+import { StateResponse } from 'src/common/swagger/classes/address.class'
 
+@ApiTags('State')
 @Controller('state')
 export class StateController {
   constructor(private readonly stateService: StateService) {}
+
   @Post()
+  @GenericResponses({ auth: true })
+  @ApiCreatedResponse({
+    description: 'Created'
+  })
+  @ApiOperation({
+    summary: 'Create a state',
+    description: Description.administrator
+  })
   @Auth(Roles.ADMINISTRATOR)
   create(@Body() createCountryCodeDto: CreateStateDto) {
-    return this.stateService.create(createCountryCodeDto)
+    this.stateService.create(createCountryCodeDto)
   }
 
   @Get()
+  @GenericResponses()
+  @ApiOkResponse({
+    description: 'Success',
+    type: [StateResponse]
+  })
+  @ApiOperation({
+    summary: 'Find all states'
+  })
+  @ApiExtraModels(PaginationDto)
   findAll(@Query() query: PaginationDto) {
     return this.stateService.findAll()
   }
 
   @Get(':id')
+  @GenericResponses()
+  @ApiNotFoundResponse({
+    description: 'Not Found'
+  })
+  @ApiOkResponse({
+    description: 'Success',
+    type: StateResponse
+  })
+  @ApiOperation({
+    summary: 'Find state by id'
+  })
   findById(@Param('id') id: string) {
     return this.stateService.findById(id)
   }
 
   @Patch(':id')
+  @GenericResponses({ auth: true })
+  @ApiNoContentResponse({
+    description: 'No content'
+  })
+  @ApiNotFoundResponse({
+    description: 'Not Found'
+  })
+  @ApiOperation({
+    summary: 'Update state by id',
+    description: Description.administrator
+  })
   @Auth(Roles.ADMINISTRATOR)
   @HttpCode(HttpStatus.NO_CONTENT)
   updateById(@Param('id') id: string, @Body() updateStateDto: UpdateStateDto) {
@@ -44,6 +97,17 @@ export class StateController {
   }
 
   @Delete(':id')
+  @GenericResponses({ auth: true })
+  @ApiNoContentResponse({
+    description: 'No content'
+  })
+  @ApiNotFoundResponse({
+    description: 'Not Found'
+  })
+  @ApiOperation({
+    summary: 'Delete state by id',
+    description: Description.administrator
+  })
   @Auth(Roles.ADMINISTRATOR)
   @HttpCode(HttpStatus.NO_CONTENT)
   deleteById(@Param('id') id: string) {
