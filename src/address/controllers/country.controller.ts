f74c5aeab37cb04be52b66c0193579c640d9ba83
Bryan Portillo <bportillo701@gmail.com>
2024-06-02 19:16:03 -0600

    MEDB-83 Added Country Documentation

--- src/address/controllers/country.controller.ts
+++ src/address/controllers/country.controller.ts
@@ -16,27 +16,80 @@ import { UpdateCountryDto } from '../dto/country/update-country.dto'
 import { Auth } from 'src/auth/decorators'
 import { PaginationDto } from 'src/common/dtos'
 import { Roles } from 'src/user/enums'
+import {
+  ApiCreatedResponse,
+  ApiExtraModels,
+  ApiNoContentResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags
+} from '@nestjs/swagger'
+import { GenericResponses } from 'src/common/decorators/genericResponses.decorator'
+import { Description } from 'src/common/swagger/description.swagger'
+import { CountryResponse } from 'src/common/swagger/classes/address.class'
 
+@ApiTags('Country')
 @Controller('country')
 export class CountryController {
   constructor(private readonly countryService: CountryService) {}
+
   @Post()
+  @GenericResponses({ auth: true })
+  @ApiCreatedResponse({
+    description: 'Created'
+  })
+  @ApiOperation({
+    summary: 'Create a country',
+    description: Description.administrator
+  })
   @Auth(Roles.ADMINISTRATOR)
   create(@Body() createCountryCodeDto: CreateCountryDto) {
-    return this.countryService.create(createCountryCodeDto)
+    this.countryService.create(createCountryCodeDto)
   }
 
   @Get()
+  @GenericResponses()
+  @ApiOkResponse({
+    description: 'Success',
+    type: [CountryResponse]
+  })
+  @ApiOperation({
+    summary: 'Find all countries'
+  })
+  @ApiExtraModels(PaginationDto)
   findAll(@Query() query: PaginationDto) {
     return this.countryService.findAll()
   }
 
   @Get(':id')
+  @GenericResponses()
+  @ApiNotFoundResponse({
+    description: 'Not Found'
+  })
+  @ApiOkResponse({
+    description: 'Success',
+    type: CountryResponse
+  })
+  @ApiOperation({
+    summary: 'Find country by id'
+  })
   findById(@Param('id') id: string) {
     return this.countryService.findById(id)
   }
 
   @Patch(':id')
+  @GenericResponses({ auth: true })
+  @ApiNoContentResponse({
+    description: 'No content'
+  })
+  @ApiNotFoundResponse({
+    description: 'Not Found'
+  })
+  @ApiOperation({
+    summary: 'Update country by id',
+    description: Description.administrator
+  })
   @Auth(Roles.ADMINISTRATOR)
   @HttpCode(HttpStatus.NO_CONTENT)
   updateById(
@@ -47,6 +100,17 @@ export class CountryController {
   }
 
   @Delete(':id')
+  @GenericResponses({ auth: true })
+  @ApiNoContentResponse({
+    description: 'No content'
+  })
+  @ApiNotFoundResponse({
+    description: 'Not Found'
+  })
+  @ApiOperation({
+    summary: 'Delete country by id',
+    description: Description.administrator
+  })
   @Auth(Roles.ADMINISTRATOR)
   @HttpCode(HttpStatus.NO_CONTENT)
   deleteById(@Param('id') id: string) {
--- src/address/dto/country/create-country.dto.ts
+++ src/address/dto/country/create-country.dto.ts
@@ -1,6 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsString, Length } from 'class-validator'
 
 export class CreateCountryDto {
+  @ApiProperty({
+    description: 'Country name',
+    example: 'Brazil',
+    minimum: 5,
+    maximum: 20
+  })
   @IsString()
   @Length(5, 20)
   name: string
